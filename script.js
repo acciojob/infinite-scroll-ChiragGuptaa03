48ec8479900afdf92a4d4f0b61681adfa6e6a9d5
Chirag Gupta <chirag4522gupta@gmail.com>
2025-12-30 14:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+
+function addItems(number) {
+  for (let i = 0; i < number; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + count;
+    list.appendChild(li);
+    count++;
+  }
+}
+
+
+addItems(10);
+
+// infinite scroll logic
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
 
